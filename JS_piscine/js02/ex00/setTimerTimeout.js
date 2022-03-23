69a407e6f42e9c0be583a96f4efa6986c360ee8c
yeokyoung song <spolice0324@gmail.com>
2022-03-23 14:04:07 +0900

    save

--- JS_piscine/js02/ex00/setTimerTimeout.js
+++ JS_piscine/js02/ex00/setTimerTimeout.js
@@ -1,3 +1,21 @@
-setTimeout(() => {
-    console.log('2초가 지났다.')
-}, 2000);
+
+export function setTimer(callback, timeout){
+    let count = 0;
+
+    function recursive(){
+        const timeoutID = setTImeout(() => {
+            let ret = callback(count);
+            if(! ret)
+                clearTimeout(timeoutID);
+            else
+                recursive();
+
+            count ++;     
+     }, timeout);
+
+    }
+
+    recursive();
+
+}
+
---
+++ JS_piscine/js02/ex02/recipe_promise.js
@@ -0,0 +1,33 @@
+const recipe = {
+
+    '1.반죽만들기': 3000,
+    '2.1차 발효': 5000,
+    '3.성형하기': 4200,
+    '4.2차 발효': 2000,
+    '5.튀기기': 5000,
+    
+};
+
+
+function baking(step) {
+    let stepName = Object.keys(recipe)[step];  //
+    if (step !== 5) {
+        setTimeout(() => {
+            try {
+                randomFail();
+                console.log(`${stepName} 성공!`);
+                baking(step + 1);
+            }
+            catch (error) { //randomfail 에 걸리면 catch로 내려옴
+                console.log(`${stepName}, ${error}`);
+                baking(step); //catch에 걸리면 baking 함수 재실행
+            }
+        }, recipe[stepName]); //트라이에 성공한 baking step을 recipe 배열에 저장
+    }
+};
+
+baking(0); //베이킹함수 무한반복 실행
+
+function randomFail() {
+    if (Math.random() < 0.2) throw "제작 실패..!(월급이 삭감되었다 ㅜㅜ)"; //랜덤적으로 0.2의 확률로 실패하게 함
+}
