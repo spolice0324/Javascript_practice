343a52056cb54a222b471a461d98b579ba6f07ad
yeokyoung song <spolice0324@gmail.com>
2022-07-06 19:10:24 +0900

    fe00

---
+++ JS_piscine/fe00/ex00/hello-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hello from './Hello';
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <Hello name = "react" color= "red"/>
+   <Hello color = "pink"/>
+    </>
+     );
+     
+}
+
+export default App;
