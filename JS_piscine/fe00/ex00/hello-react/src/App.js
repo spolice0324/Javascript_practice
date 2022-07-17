fc54ea510ab52af5dd1b5c1fce05a61f79680abd
yeokyoung song <spolice0324@gmail.com>
2022-07-17 18:51:16 +0900

    update

--- JS_piscine/fe00/ex00/hello-react/src/App.js
+++ JS_piscine/fe00/ex00/hello-react/src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
-import Counter from './Counter';
 import InputSample from './InputSample';
 
 function App() {
   return (
-   <Wrapper>
-   <Hello name = "react" color= "red" isSpecial={true}/>
-   <Hello color = "pink"/>
-   <Counter />
-   <InputSample />
-    </Wrapper>
-
+   
+    <InputSample />
      );
      
 }
--- JS_piscine/fe00/ex00/hello-react/src/Hello.js
+++ JS_piscine/fe00/ex00/hello-react/src/Hello.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Hello( {color, name, isSpecial} ){
   return (
   <div style= {{color}}>
-    {isSpecial ? <b>*</b> : null} 
-     안녕하세요 {name}
-    / </div>  
+    {isSpecial ? <b> * </b> : null} 
+     안녕하세요 {name}님
+     </div>  
   );
 }
 
