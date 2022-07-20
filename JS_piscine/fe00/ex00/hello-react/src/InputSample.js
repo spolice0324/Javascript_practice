61fb4f45731d8ec926ad3580ae97f21591c398ac
yeokyoung song <spolice0324@gmail.com>
2022-07-20 23:03:47 +0900

    s

--- JS_piscine/fe00/ex00/hello-react/src/InputSample.js
+++ JS_piscine/fe00/ex00/hello-react/src/InputSample.js
@@ -1,24 +1,37 @@
 import React, {useState} from 'react';
 
 function InputSample() {
+    const [inputs, setInputs] = useState({
+        name: '',
+        nick: '',
+    });
 
+    const {name, nickname } = inputs;
 
     const onChange = (e) => {
-
+        const {value, name } = e.target;
+        setInputs({
+            ...inputs,
+            [name] : value,
+        });
     };
 
     const onReset = () => {
-
+        setInputs({
+            name: '',
+            nickname: '',
+        })
     };
+
 
     return (
         <div>
-            <input placeholder="이름" />
-            <input placeholder="닉네임"/>
+            <input name="name" placeholder="이름" onChange={onChange} value={name} />
+            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
-                이름 (닉네임)
+              {name} ({nickname})
             </div>
         </div>
     );
